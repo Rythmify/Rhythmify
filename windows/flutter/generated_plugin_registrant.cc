b5e924850e59704be7f2e00142a1157f6b266b8e
biso <basselalaa33@gmail.com>
2026-03-15 00:25:09 +0200

    Solved Conflicts

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -9,10 +9,7 @@
 #include <app_links/app_links_plugin_c_api.h>
 #include <file_selector_windows/file_selector_windows.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
-<<<<<<< HEAD
-=======
 #include <share_plus/share_plus_windows_plugin_c_api.h>
->>>>>>> de9aba3f320e3c553e5f7159c0d6a45d71a35460
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -22,11 +19,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
-<<<<<<< HEAD
-=======
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
->>>>>>> de9aba3f320e3c553e5f7159c0d6a45d71a35460
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
