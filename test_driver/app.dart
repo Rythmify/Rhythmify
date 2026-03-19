b9149d0d71578a0b620374effb203e4e922d18ca
bisolabs <basselalaa33@gmail.com>
2026-03-19 17:09:30 +0200

    Allowed The testing Driver

---
+++ test_driver/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:rythmify/main.dart' as app;
+void main() {
+  enableFlutterDriverExtension();
+
+  // This calls your app's actual main function
+  app.main();
+}
