5902150b8cacb68d5c0327f4a7220bc58c0e92a7
biso <basselalaa33@gmail.com>
2026-04-12 11:32:05 +0200

    Feat: implemented the waveform integration successfully

--- lib/features/track/data/repositories/track_repository_impl.dart
+++ lib/features/track/data/repositories/track_repository_impl.dart
@@ -30,7 +30,14 @@ class TrackRepositoryImpl implements TrackRepository {
   @override
   Future<List<double>> getWaveform(String trackId) async {
     final response = await remoteDataSource.getWaveform(trackId);
-    final List<dynamic> peaks = response['data']['peaks'];
+    
+    List<dynamic> peaks = [];
+    if (response.containsKey('data') && response['data'] is Map && response['data'].containsKey('peaks')) {
+      peaks = response['data']['peaks'];
+    } else if (response.containsKey('peaks')) {
+      peaks = response['peaks'];
+    }
+    
     return peaks.map((p) => (p as num).toDouble()).toList();
   }
 
