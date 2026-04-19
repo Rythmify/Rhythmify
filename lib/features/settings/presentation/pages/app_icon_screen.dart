9a03f32ff70282d8c3dc2d9775643100d819d5fc
RanaElgharabawy <rana.samir05@eng-st.cu.edu.eg>
2026-04-19 20:57:09 +0200

    app icon change

--- lib/features/settings/presentation/pages/app_icon_screen.dart
+++ lib/features/settings/presentation/pages/app_icon_screen.dart
@@ -20,14 +20,14 @@ class _IconOption {
 
 const List<_IconOption> _icons = [
   _IconOption(name: 'Default', assetPath: 'assets/icons/app_icon.png'),
-  _IconOption(name: 'OG', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconOG'),
-  _IconOption(name: 'Chrome', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconChrome', isPremium: true),
-  _IconOption(name: 'Rose Gold', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconRoseGold', isPremium: true),
-  _IconOption(name: 'Silver', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconSilver', isPremium: true),
-  _IconOption(name: 'Soft Purple', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconSoftPurple', isPremium: true),
-  _IconOption(name: 'Hot Pink', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconHotPink', isPremium: true),
-  _IconOption(name: 'Tie-Dye', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconTieDye', isPremium: true),
-  _IconOption(name: 'Leopard', assetPath: 'assets/icons/app_icon.png', iconKey: 'AppIconLeopard', isPremium: true),
+  _IconOption(name: 'OG', assetPath: 'assets/icons/app_icon_og.png', iconKey: 'AppIconOG'),
+  _IconOption(name: 'Chrome', assetPath: 'assets/icons/app_icon_chrome.png', iconKey: 'AppIconChrome', isPremium: true),
+  _IconOption(name: 'Rose Gold', assetPath: 'assets/icons/app_icon_rose_gold.png', iconKey: 'AppIconRoseGold', isPremium: true),
+  _IconOption(name: 'Silver', assetPath: 'assets/icons/app_icon_silver.png', iconKey: 'AppIconSilver', isPremium: true),
+  _IconOption(name: 'Soft Purple', assetPath: 'assets/icons/app_icon_soft_purple.png', iconKey: 'AppIconSoftPurple', isPremium: true),
+  _IconOption(name: 'Hot Pink', assetPath: 'assets/icons/app_icon_hot_pink.png', iconKey: 'AppIconHotPink', isPremium: true),
+  _IconOption(name: 'Tie-Dye', assetPath: 'assets/icons/app_icon_tie_dye.png', iconKey: 'AppIconTieDye', isPremium: true),
+  _IconOption(name: 'Leopard', assetPath: 'assets/icons/app_icon_leopard.png', iconKey: 'AppIconLeopard', isPremium: true),
 
 ];
 
@@ -64,13 +64,13 @@ class _AppIconScreenState extends State<AppIconScreen> {
     if (_selectedKey == option.iconKey) return;
 
     try {
-      await DynamicAppIconFlutterPlus.setAlternateIconName(option.iconKey);
       final prefs = await SharedPreferences.getInstance();
       if (option.iconKey == null) {
         await prefs.remove(_prefKey);
       } else {
         await prefs.setString(_prefKey, option.iconKey!);
       }
+      await DynamicAppIconFlutterPlus.setAlternateIconName(option.iconKey);
       if (mounted) setState(() => _selectedKey = option.iconKey);
     } catch (_) {
       if (mounted) {
